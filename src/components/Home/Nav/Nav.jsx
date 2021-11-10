73c923537306dfa4f9ccee00ec0938c0946ea200
azadtamal <mrazadt@gmail.com>
2021-11-10 15:32:18 +0600

    footer

--- src/components/Home/Nav/Nav.jsx
+++ src/components/Home/Nav/Nav.jsx
@@ -6,52 +6,49 @@ const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 py-2 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <div className="flex-shrink-0 text-white">
-              {/* <img className="h-8 w-8" src="" alt="logo" /> */}
-              AZAD
-            </div>
+            <div className="flex-shrink-0 text-3xl text-white">AZAD</div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link
                   to="/home"
-                  className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-lg font-medium"
                 >
                   Home
                 </Link>
                 <Link
                   to="/news"
-                  className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-lg font-medium"
                 >
                   News
                 </Link>
 
                 <Link
                   to="/moreCars"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
                 >
                   More Cars
                 </Link>
 
                 <Link
                   to="/dashboard"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
                 >
                   Dashboard
                 </Link>
 
                 <Link
                   to="/login"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
                 >
                   Login
                 </Link>
 
                 <Link
                   to="/"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
                 >
                   Logout
                 </Link>
