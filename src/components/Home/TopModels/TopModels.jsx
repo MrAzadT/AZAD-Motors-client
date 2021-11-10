b96d505425cd6efcfe7b5200961e6bb259cb37b0
azadtamal <mrazadt@gmail.com>
2021-11-10 21:17:14 +0600

    data

--- src/components/Home/TopModels/TopModels.jsx
+++ src/components/Home/TopModels/TopModels.jsx
@@ -4,49 +4,52 @@ import "./TopModels.css";
 const TopModels = () => {
   return (
     <>
-      <h1>The Most Iconic Cars</h1>
+      <h1 className="text-center  text-4xl mt-14 mb-3 ">
+        The Most Iconic Cars
+      </h1>
+      <hr className="border-2 w-80 m-auto mb-10 border-red-400" />
       <div className=" w-5/6 m-auto ">
         <div className="gallery">
           <figure className="gallery__item gallery__item--1">
             <img
               src="https://res.cloudinary.com/abidazad/image/upload/v1636541082/pexels-den-pilsh-8022352_1_bamukg.jpg"
               alt="img"
-              className="gallery__img"
+              className="gallery__img rounded-lg"
             />
           </figure>
           <figure className="gallery__item gallery__item--2">
             <img
               src="https://res.cloudinary.com/abidazad/image/upload/v1636541083/pexels-mike-9735310_1_xs6wuq.jpg"
               alt="img"
-              className="gallery__img"
+              className="gallery__img rounded-lg"
             />
           </figure>
           <figure className="gallery__item gallery__item--3">
             <img
               src="https://res.cloudinary.com/abidazad/image/upload/v1636541082/pexels-luca-sammarco-6194412_1_w6fsg0.jpg"
               alt="img"
-              className="gallery__img"
+              className="gallery__img rounded-lg"
             />
           </figure>
           <figure className="gallery__item gallery__item--4">
             <img
               src="https://res.cloudinary.com/abidazad/image/upload/v1636541080/pexels-abdulwahab-alawadhi-3422964_qtl4dq.jpg"
               alt="img"
-              className="gallery__img"
+              className="gallery__img rounded-lg"
             />
           </figure>
           <figure className="gallery__item gallery__item--5">
             <img
               src="https://res.cloudinary.com/abidazad/image/upload/v1636541081/pexels-denys-gromov-4635349_1_xohuno.jpg"
               alt="img"
-              className="gallery__img"
+              className="gallery__img rounded-lg"
             />
           </figure>
           <figure className="gallery__item gallery__item--6">
             <img
               src="https://res.cloudinary.com/abidazad/image/upload/v1636541078/pexels-jae-park-3764984_1_ws8d9q.jpg"
               alt="img"
-              className="gallery__img"
+              className="gallery__img rounded-lg"
             />
           </figure>
         </div>
