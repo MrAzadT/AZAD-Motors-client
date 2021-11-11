b1015df15fb0c5e144ee3cb8986e2078e1307009
azadtamal <mrazadt@gmail.com>
2021-11-11 10:48:47 +0600

    <AuthProvider>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AuthProvider from "./components/Context/AuthProvider";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Footer from "./components/Home/Footer/Footer";
-
 import Home from "./components/Home/Home/Home";
 import Nav from "./components/Home/Nav/Nav";
 import Login from "./components/Login/Login/Login";
@@ -15,39 +14,41 @@ import NotFound from "./components/NotFound/NotFound";
 function App() {
   return (
     <>
-      <Router>
-        <Nav />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/news">
-            <News />
-          </Route>
-          <Route exact path="/news">
-            <News />
-          </Route>
-          <Route exact path="/moreCars">
-            <MoreCars />
-          </Route>
-          <Route exact path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/signUp">
-            <SignUp />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-        <Footer />
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Nav />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+            <Route exact path="/moreCars">
+              <MoreCars />
+            </Route>
+            <Route exact path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/signUp">
+              <SignUp />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </AuthProvider>
     </>
   );
 }
