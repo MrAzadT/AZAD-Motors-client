25d5dbdb2318bd4d07a3a58915431dd687bc34f8
azadtamal <mrazadt@gmail.com>
2021-11-12 15:40:36 +0600

    post cars

--- src/components/Dashboard/Dashboard/AddProduct.jsx
+++ src/components/Dashboard/Dashboard/AddProduct.jsx
@@ -15,7 +15,7 @@ const AddProduct = () => {
     console.log(data);
     data.email = currentUser.email;
     axios
-      .post("http://localhost:5000/orders", data)
+      .post("http://localhost:5000/cars", data)
       .then((res) => {
         console.log(res);
       })
@@ -27,27 +27,34 @@ const AddProduct = () => {
   return (
     <div className="w-full text-center mt-16 m-auto">
       <h1 className=" text-3xl r font-bold text-green-600">Add A Product</h1>
-      <hr className="border-2 w-40 m-auto mt-2 mb-5 border-green-700" />
+      <hr className="border-2 w-52 m-auto mt-2 mb-5 border-green-700" />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label className="block mt-3 text-md mb-2">Name</label>
+        <label className="block mt-3 text-md mb-2">Image Link</label>
         <input
-          placeholder="name"
+          placeholder="image link"
           className="px-4 w-80 ring-2 ring-green-400 border-2 py-2 rounded-md text-sm outline-none"
-          {...register("name", { required: true })}
+          {...register("img", { required: true })}
         />
         <br />
-        <label className="block mt-3 text-md mb-2">Email</label>
+        <label className="block mt-3 text-md mb-2">Company</label>
         <input
-          placeholder="email"
+          placeholder="company"
           className="px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none"
-          {...register("email", { required: true })}
+          {...register("company", { required: true })}
         />
         <br />
-        <label className="block mt-3 text-md mb-2">Car Name</label>
+        <label className="block mt-3 text-md mb-2">Price</label>
         <input
-          placeholder="carName"
+          placeholder="price"
           className="px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none"
-          {...register("carName", { required: true })}
+          {...register("price", { required: true })}
+        />
+        <br />
+        <label className="block mt-3 text-md mb-2">Tax</label>
+        <input
+          placeholder="tax"
+          className="px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none"
+          {...register("tax", { required: true })}
         />
         <br />
         <label className="block mt-3 text-md mb-2">Model</label>
