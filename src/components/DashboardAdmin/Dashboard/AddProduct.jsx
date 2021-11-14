60977cd3c6aa521cecce3ee4401a9cd8bd02e232
azadtamal <mrazadt@gmail.com>
2021-11-14 11:35:59 +0600

    100% done

--- src/components/DashboardAdmin/Dashboard/AddProduct.jsx
+++ src/components/DashboardAdmin/Dashboard/AddProduct.jsx
@@ -1,86 +1,85 @@
-import axios from 'axios'
-import React, { useContext } from 'react'
-import { useForm } from 'react-hook-form'
-import { AuthContext } from '../../Context/AuthProvider'
+import axios from "axios";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddProduct = () => {
-  const { currentUser } = useContext(AuthContext)
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data)
-    // data.email = currentUser.email;
+    console.log(data);
+
     axios
-      .post('http://localhost:5000/orderDelete/cars', data)
+      .post("https://limitless-temple-20432.herokuapp.com/cars", data)
       .then((res) => {
-        console.log(res)
+        console.log(res);
+        alert("Add a Product successful ");
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }
+        console.log(err);
+      });
+  };
 
   return (
-    <div className='w-full text-center mt-16 m-auto'>
-      <h1 className=' text-3xl r font-bold text-green-600'>Add A Product</h1>
-      <hr className='border-2 w-52 m-auto mt-2 mb-5 border-green-700' />
+    <div className="w-full text-center mt-16 m-auto">
+      <h1 className=" text-3xl r font-bold text-green-600">Add A Product</h1>
+      <hr className="border-2 w-52 m-auto mt-2 mb-5 border-green-700" />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label className='block mt-3 text-md mb-2'>Image Link</label>
+        <label className="block mt-3 text-md mb-2">Image Link</label>
         <input
-          placeholder='image link'
-          className='px-4 w-80 ring-2 ring-green-400 border-2 py-2 rounded-md text-sm outline-none'
-          {...register('img', { required: true })}
+          placeholder="image link"
+          className="px-4 w-80 ring-2 ring-green-400 border-2 py-2 rounded-md text-sm outline-none"
+          {...register("img", { required: true })}
         />
         <br />
-        <label className='block mt-3 text-md mb-2'>Company</label>
+        <label className="block mt-3 text-md mb-2">Company</label>
         <input
-          placeholder='company'
-          className='px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none'
-          {...register('company', { required: true })}
+          placeholder="company"
+          className="px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none"
+          {...register("company", { required: true })}
         />
         <br />
-        <label className='block mt-3 text-md mb-2'>Price</label>
+        <label className="block mt-3 text-md mb-2">Price</label>
         <input
-          placeholder='price'
-          className='px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none'
-          {...register('price', { required: true })}
+          placeholder="price"
+          className="px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none"
+          {...register("price", { required: true })}
         />
         <br />
-        <label className='block mt-3 text-md mb-2'>Tax</label>
+        <label className="block mt-3 text-md mb-2">Tax</label>
         <input
-          placeholder='tax'
-          className='px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none'
-          {...register('tax', { required: true })}
+          placeholder="tax"
+          className="px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none"
+          {...register("tax", { required: true })}
         />
         <br />
-        <label className='block mt-3 text-md mb-2'>Model</label>
+        <label className="block mt-3 text-md mb-2">Model</label>
         <input
-          placeholder='model'
-          className='px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none'
-          {...register('model', { required: true })}
+          placeholder="model"
+          className="px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none"
+          {...register("model", { required: true })}
         />
         <br />
-        <label className='block mt-3 text-md mb-2'>Details</label>
+        <label className="block mt-3 text-md mb-2">Details</label>
         <input
-          placeholder='details'
-          className='px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none'
-          {...register('dis', { required: true })}
+          placeholder="details"
+          className="px-4 w-80 ring-green-400 ring-2 border-2 py-2 rounded-md text-sm outline-none"
+          {...register("dis", { required: true })}
         />
         <br />
 
         {errors.exampleRequired && <span>This field is required</span>}
         <br />
         <input
-          className='mt-4 cursor-pointer px-4 ring-2 ring-green-500 py-2 border-2  rounded-lg bg-green-400 text-white'
-          type='submit'
+          className="mt-4 cursor-pointer px-4 ring-2 ring-green-500 py-2 border-2  rounded-lg bg-green-400 text-white"
+          type="submit"
         />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
--- src/components/DashboardAdmin/Dashboard/ManageProducts.jsx
+++ src/components/DashboardAdmin/Dashboard/ManageProducts.jsx
@@ -1,48 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { AuthContext } from "./../../Context/AuthProvider";
 
 const ManageProducts = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+  const [orderChange, setOrderChanged] = useState(false);
 
   useEffect(() => {
     axios
-      .get('http://localhost:5000/orderDelete/cars')
+      .get("https://limitless-temple-20432.herokuapp.com/cars")
       .then((res) => {
-        console.log(res.data)
-        setData(res.data)
+        console.log(res.data);
+        setData(res.data);
       })
       .catch((err) => {
-        console.log(err)
+        console.log(err);
+      });
+  }, [currentUser, orderChange]);
+
+  const handleDelete = (id) => {
+    axios
+      .delete(
+        `https://limitless-temple-20432.herokuapp.com/productDelete/${id}`
+      )
+      .then((res) => {
+        console.log(res);
+        setOrderChanged((prev) => !prev);
       })
-  }, [])
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
-      <div className='grid xl:grid-cols-3 gap-10 m-auto mt-14 md:grid-cols-2 grid-cols-1 max-w-6xl'>
+      <h1 className="w-full m-auto text-center text-3xl font-bold mt-11">
+        Manage ALL Product
+      </h1>
+      <div className="grid xl:grid-cols-3 gap-10 m-auto mt-14 md:grid-cols-2 grid-cols-1 max-w-6xl">
         {data.map((item) => (
-          <div key={item.id} className=' w-80 mx-auto text-center '>
-            <div className='bg-white shadow-md translate border border-gray-200 rounded-lg max-w-sm mb-5'>
+          <div key={item.id} className=" w-80 mx-auto text-center ">
+            <div className="bg-white shadow-md translate border border-gray-200 rounded-lg max-w-sm mb-5">
               <img
-                className=' rounded-lg w-80 rounded-lgv h-60'
+                className=" rounded-lg w-80 rounded-lgv h-60"
                 src={item.img}
-                alt=''
+                alt=""
               />
 
-              <div className='p-5'>
-                <h5 className='text-gray-900 font-bold text-2xl tracking-tight mb-2'>
+              <div className="p-5">
+                <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
                   Car Name : {item.company}
                 </h5>
 
-                <p className='font-normal text-xl text-gray-700 mb-3'>
+                <p className="font-normal text-xl text-gray-700 mb-3">
                   Model : {item.model}
                 </p>
-                <p className='font-normal text-lg text-gray-700 mb-3'>
+                <p className="font-normal text-lg text-gray-700 mb-3">
                   Price : ${item.price}
                 </p>
-                <p className='font-normal text-gray-700 mb-3'>
+                <p className="font-normal text-gray-700 mb-3">
                   Tax : {item.tax}
                 </p>
-                <button className='text-white ring-2 ring-blue-500 bg-blue-700 hover:bg-blue-800 focus:ring-4  font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center'>
+                <button
+                  onClick={() => handleDelete(item._id)}
+                  className="text-white ring-2 ring-blue-500 bg-blue-700 hover:bg-blue-800 focus:ring-4  font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
+                >
                   DELETE PRODUCT
                 </button>
               </div>
@@ -51,7 +74,7 @@ const ManageProducts = () => {
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ManageProducts
+export default ManageProducts;
